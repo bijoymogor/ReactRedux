b8c75ab4724282cbed1b66b7adce05c9c1163d8b
Bijoy <bijoymogor@gmail.com>
2023-04-09 23:53:19 +0530

    Subscription created with store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,26 @@
 import classes from './Counter.module.css';
+import { useSelector } from 'react-redux';
+
 
 const Counter = () => {
+  const counter = useSelector(state=> state.counter);    //Works like state automatically re render component on change
+
+  // console.log(counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      
+      <div>
+        <button>Increment</button>
+        <button>Decrement</button>
+
+
+      </div>
+
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
